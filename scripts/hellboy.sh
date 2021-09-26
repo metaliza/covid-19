455d12b0887e3b77ee1f8ab3b3bf44154169d2c5
Tomáš Metelka <tomas.metelka@metaliza.cz>
2021-09-26 20:02:11 +0200

    Hellboy's download script for "advanced" data

---
+++ scripts/hellboy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+wget -O okresy_stat.csv https://onemocneni-aktualne.mzcr.cz/api/account/stbkltONzRu3wstWR0dxdBsL0g/file/modely%252Fmodely_01_okresni_statistika.csv
+
+wget -O efektivita_testu.csv https://onemocneni-aktualne.mzcr.cz/api/account/stbkltONzRu3wstWR0dxdBsL0g/file/modely%252Fmodely_02_efektivita_testovani.csv
+
+wget -O testy_poz.csv https://onemocneni-aktualne.mzcr.cz/api/account/stbkltONzRu3wstWR0dxdBsL0g/file/modely%252Fmodely_03_testy_pozitivnich.csv
+
+wget -O hospitalizace.csv https://onemocneni-aktualne.mzcr.cz/api/account/stbkltONzRu3wstWR0dxdBsL0g/file/modely%252Fmodely_04_hospitalizovani.csv
+
+wget -O hospitalizace_analyza.csv https://onemocneni-aktualne.mzcr.cz/api/account/stbkltONzRu3wstWR0dxdBsL0g/file/modely%252Fmodely_05_hospitalizovani_analyza.csv
+
+#wget -O okresy_stat.csv https://onemocneni-aktualne.mzcr.cz/api/account/stbkltONzRu3wstWR0dxdBsL0g/file/modely%252Fmodely_readme.txt
+
